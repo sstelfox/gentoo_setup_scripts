4615bdb00cb0b8befd4d4719c0540410fa52fa7f
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2018-08-06 20:54:30 -0400

    Refine logic on gentoo mirror selection

--- 000_mirror_select.sh
+++ 000_mirror_select.sh
@@ -3,6 +3,8 @@
 . ./_config.sh
 . ./_error_handling.sh
 
-if [ "${LOCAL}" != "yes" -a -z "${GENTOO_MIRRORS:-}" ]; then
-  mirrorselect -o -s 1 -q -D -H -R 'North America' 2> /dev/null >> ./_config.sh
+if [ "${LOCAL}" != "yes" ]; then
+  if [ -z "${GENTOO_MIRRORS:-}" ]; then
+    mirrorselect -o -s 1 -q -D -H -R 'North America' 2> /dev/null >> ./_config.sh
+  fi
 fi
--- nfs_start.sh
+++ nfs_start.sh
@@ -9,3 +9,5 @@ fi
 
 docker run -d --rm --name gentoo_nfs --privileged -v $(pwd):/gentoo_cache \
   -p 2049:2049 -e SHARED_DIRECTORY=/gentoo_cache itsthenetwork/nfs-server-alpine:latest
+
+sudo iptables -A INPUT -m tcp -p tcp --dport 2049 -i virbr0 -j ACCEPT
