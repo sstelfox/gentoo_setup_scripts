1235cc03584994166c340e21d4ebcb6d0dccecd3
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2018-08-14 10:41:15 -0400

    Test changing the order of system profile selection...

--- 013_setup_system_profile.sh
+++ 013_setup_system_profile.sh
@@ -3,18 +3,3 @@
 . ./_config.sh
 . ./_error_handling.sh
 
-PROFILE_ID=$(
-  chroot /mnt/gentoo eselect profile list | \
-  grep 'hardened/linux/amd64/no-multilib/selinux' | \
-  awk '{ print $1 }' | \
-  grep -oE '[0-9]+'
-)
-chroot /mnt/gentoo eselect profile set ${PROFILE_ID}
-
-chroot /mnt/gentoo emerge --update --newuse --deep @world
-chroot /mnt/gentoo emerge @preserved-rebuild
-
-# This may need to have the selinux feature disabled, and may need to
-# be || true
-FEATURES="-selinux" chroot /mnt/gentoo emerge sec-policy/selinux-base sys-kernel/linux-firmware \
-  sec-policy/selinux-base-policy
