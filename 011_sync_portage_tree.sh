b74fb2ae2e4f4948d28d5422ba51cf8426492266
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2018-08-14 20:28:52 -0400

    Not ideal but switch it back...

--- 011_sync_portage_tree.sh
+++ 011_sync_portage_tree.sh
@@ -16,20 +16,7 @@ fi
 chroot /mnt/gentoo eselect news read all --quiet
 chroot /mnt/gentoo eselect news purge
 
-PROFILE_ID=$(
-  chroot /mnt/gentoo eselect profile list | \
-  grep 'hardened/linux/amd64/no-multilib/selinux' | \
-  awk '{ print $1 }' | \
-  grep -oE '[0-9]+'
-)
-chroot /mnt/gentoo eselect profile set ${PROFILE_ID}
-
 # Future installs may incompatible, make sure we're up to date before we
 # attempt to hit important things like the kernel.
 chroot /mnt/gentoo emerge --update --newuse --deep @world
 chroot /mnt/gentoo emerge @preserved-rebuild
-
-# This may need to have the selinux feature disabled, and may need to
-# be || true
-FEATURES="-selinux" chroot /mnt/gentoo emerge sec-policy/selinux-base sys-kernel/linux-firmware \
-  sec-policy/selinux-base-policy
