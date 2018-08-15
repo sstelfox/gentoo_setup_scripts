291f63624738c20d8f31dfec5e2e24fdab2485d0
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2018-08-14 23:54:49 -0400

    I need to ensure curl is present

--- 016_install_system_packages.sh
+++ 016_install_system_packages.sh
@@ -3,7 +3,7 @@
 . ./_config.sh
 . ./_error_handling.sh
 
-chroot /mnt/gentoo emerge sys-apps/dmidecode app-arch/lz4 \
+chroot /mnt/gentoo emerge sys-apps/dmidecode app-arch/lz4 net-misc/curl \
   sys-apps/lm_sensors sys-apps/usbutils sys-apps/pciutils sys-block/parted \
   sys-fs/cryptsetup sys-fs/dosfstools sys-fs/lvm2 sys-fs/xfsprogs \
   sys-kernel/dracut
