d1fb4e9ef169bbdfdd54d3f13385cbb4fd861cbe
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2018-08-16 21:49:58 -0400

    Make the serial console more intelligent

--- 024_config_init_system.sh
+++ 024_config_init_system.sh
@@ -4,7 +4,16 @@
 . ./_error_handling.sh
 
 sed -i '/ctrlaltdel/d' /mnt/gentoo/etc/inittab
-echo 's0:12345:respawn:/sbin/agetty -L 115200 ttyS0 vt100' >> /mnt/gentoo/etc/inittab
+
+# There is some kind of issue with KVM hosts exposing a serial console even
+# when it isn't actually present, so don't setup this terminal on those hosts.
+if [ -c /dev/ttyS0 -a "${KERNEL_CONFIG}" != "kvm" ]; then
+  echo 's0:12345:respawn:/sbin/agetty -L 115200 ttyS0 vt100' >> /mnt/gentoo/etc/inittab
+fi
+
+if [ -c /dev/hvc0 ]; then
+  echo 'vs0:12345:respawn:/sbin/agetty -L 115200 hvc0 vt100' >> /mnt/gentoo/etc/inittab
+fi
 
 cat << 'EOF' > /mnt/gentoo/etc/rc.conf
 # /etc/rc.conf
