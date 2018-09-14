63ab021b4cb09362413f5c54a57dd80b5e24feaf
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2018-09-13 20:54:38 -0400

    Fix the NFS mount options and comments

--- 038_nfs.sh
+++ 038_nfs.sh
@@ -16,9 +16,9 @@ chroot /mnt/gentoo emerge net-fs/nfs-utils
 # TODO: Once kerberos is setup I should set the sec mode to 'krb5p'. I may need
 # to remove 'noexec' from this... When not on a build server, I should probably
 # have this as read only as well.
-echo '192.168.122.1:/cache      /usr/portage  nfs4  ro,noatime,nodev,noexec,nosuid  0 0' >> /mnt/chroot/etc/fstab
+echo '192.168.122.1:/cache      /usr/portage  nfs4  ro,noatime,nodev,noexec,nosuid  0 0' >> /mnt/gentoo/etc/fstab
 
-# The following should be the build host export:
+# The following should be the build host export (ideally with sec=krb5p for integrity and encryption)
 # /etc/exports
 #
 # /srv/build/stable    2604:a880:800:10::/48(ro,no_subtree_check,root_squash)    10.0.0.0/8(ro,no_subtree_check,root_squash)
