5ace5656bc7fd6e88acbec57840dd635e9939003
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2019-01-18 04:49:52 -0500

    update profile name and remove ID dance

--- 013_setup_system_profile.sh
+++ 013_setup_system_profile.sh
@@ -3,14 +3,7 @@
 . ./_config.sh
 . ./_error_handling.sh
 
-PROFILE_ID=$(
-  chroot /mnt/gentoo eselect profile list | \
-  grep 'hardened/linux/amd64/no-multilib/selinux' | \
-  awk '{ print $1 }' | \
-  grep -oE '[0-9]+'
-)
-chroot /mnt/gentoo eselect profile set ${PROFILE_ID}
-
+chroot /mnt/gentoo eselect profile set default/linux/amd64/17.0/no-multilib/hardened/selinux
 chroot /mnt/gentoo emerge --update --newuse --deep @world
 chroot /mnt/gentoo emerge @preserved-rebuild
 
