5364301ff5869f084a2b52ab4ddeafdefc4b20bf
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2019-02-23 23:47:27 -0500

    Remove unecessary package from the base install

--- 016_install_system_packages.sh
+++ 016_install_system_packages.sh
@@ -7,3 +7,6 @@ chroot /mnt/gentoo emerge sys-apps/dmidecode app-arch/lz4 net-misc/curl \
   sys-apps/lm_sensors sys-apps/usbutils sys-apps/pciutils sys-block/parted \
   sys-fs/cryptsetup sys-fs/dosfstools sys-fs/lvm2 sys-fs/xfsprogs \
   sys-kernel/dracut
+
+# Remove some unecessary packages
+chroot /mnt/gentoo emerge -D net-misc/dhcp
