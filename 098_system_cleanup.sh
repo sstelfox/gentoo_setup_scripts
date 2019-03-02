8341866706e45173bc50239bba84daddb8aa7c9b
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2019-03-01 19:23:12 -0500

    Clean out unecessary packages at the end of the run

--- 098_system_cleanup.sh
+++ 098_system_cleanup.sh
@@ -3,6 +3,7 @@
 . ./_config.sh
 . ./_error_handling.sh
 
+chroot /mnt/gentoo emerge --depclean
 chroot /mnt/gentoo eselect news read all > /dev/null
 chroot /mnt/gentoo find /etc -iname '._cfg*' -exec rm {} \;
 
