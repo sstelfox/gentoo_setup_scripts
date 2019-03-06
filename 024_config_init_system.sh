b6838aeeb2283bde96b563f049437c3dbeb64e84
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2019-03-06 16:13:47 -0500

    More agetty notes

--- 024_config_init_system.sh
+++ 024_config_init_system.sh
@@ -7,8 +7,16 @@
 sed -i '/ctrlaltdel/d' /mnt/gentoo/etc/inittab
 
 # This doesn't seem to be necessary as the serial console seems to be setup by
-# default now. I may want to switch the terminal type from vt100 to linux at
-# some point but this should be fine
+# default now. I may want to switch the terminal type from vt100 to vt102 or
+# linux at some point but this should be fine
+#
+# * I will likely want to add '-w -t 60' to the agetty arguments
+# * I could also try using '-n' to force the login problem to handle everything
+#   (maybe?)
+# * If I wanted to allow automatic login (since local accounts don't have
+#   passwords by default here, or any other reason). I can use:
+#   `/sbin/agetty -L --autologin root 115200 ttyS0 vt100`
+#
 #if [ "${KERNEL_CONFIG}" = "kvm" ]; then
 #  echo 's0:12345:respawn:/sbin/agetty -L 115200 ttyS0 vt100' >> /mnt/gentoo/etc/inittab
 #fi
