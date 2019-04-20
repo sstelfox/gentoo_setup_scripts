192c3c8789b39088056f49dc845e4eea05fca939
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2019-04-19 20:50:27 -0400

    Fix missing options for my standard nftables ruleset

--- kernel_config/sections/016_firewall_support.sh
+++ kernel_config/sections/016_firewall_support.sh
@@ -10,13 +10,20 @@ kernel_config --enable NETFILTER_ADVANCED
 #kernel_config --enable IP_SET
 #kernel_config --enable IP_SET_HASH_IP
 
-# TODO: Generally for the firewalls I make use of, I don't actually need
-# connection tracking, but some other features may depend on it...
-kernel_config --disable NF_CONNTRACK
+# TODO: I need to test whether or not this is a default or not as I needed to
+# adjust it after my menu run.
+kernel_config --enable NF_CONNTRACK
+
+kernel_config --disable NF_CONNTRACK_PROCFS
+kernel_config --disable NF_CT_PROTO_DCCP
+kernel_config --disable NF_CT_PROTO_SCTP
+kernel_config --disable NF_CT_PROTO_UDPLITE
 
 # Personal preference or livin' in the future, you decide but I only support
 # nftables on my systems at this point
 kernel_config --enable NF_TABLES
+kernel_config --enable NF_COUNTER
+kernel_config --enable NF_CT
 kernel_config --enable NF_TABLES_SET
 kernel_config --enable NF_TABLES_INET
 kernel_config --enable NFT_LOG
@@ -30,6 +37,7 @@ kernel_config --disable NETFILTER_XT_MATCH_ADDRTYPE
 
 # A couple of options that I may want in the future
 #kernel_config --enable NFT_LIMIT
+#kernel_config --enable NFT_NAT
 #kernel_config --enable NFT_SOCKET
 #kernel_config --enable NFT_TUNNEL
 #kernel_config --enable NFT_XFRM
