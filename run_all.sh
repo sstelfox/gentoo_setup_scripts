b46f047142039f8cb929b3a77379ed54a318eea3
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2020-02-16 10:40:02 -0600

    when running all scripts, prefix all of the output with the current segment for status updates

--- run_all.sh
+++ run_all.sh
@@ -2,8 +2,13 @@
 
 . ./_error_handling.sh
 
+function prefix_output() {
+  local PREFIX="${1:-unknown_segment}"
+  awk "{ print \"[${PREFIX}]\", \$0 }"
+}
+
 for segment in $(ls 0*.sh | sort -n); do
   echo "Executing segment: ${segment}"
-  ./${segment}
+  ./${segment} | prefix_output "${segment%%.sh}"
   echo "Segment complete"
 done
