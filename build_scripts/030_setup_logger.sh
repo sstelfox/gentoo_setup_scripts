741a3f426139591a054d6ebfa4cae1a58b4721b9
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2020-02-20 00:34:23 -0600

    reorder services, add logrotate, still need to configure it

--- build_scripts/030_setup_logger.sh
+++ build_scripts/030_setup_logger.sh
@@ -8,7 +8,7 @@
 mkdir -p /mnt/gentoo/etc/portage/package.use
 echo 'app-admin/syslog-ng json' > /mnt/gentoo/etc/portage/package.use/syslog-ng
 
-chroot /mnt/gentoo emerge app-admin/syslog-ng
+chroot /mnt/gentoo emerge app-admin/logrotate app-admin/syslog-ng
 chroot /mnt/gentoo rc-update add syslog-ng default
 
 chroot /mnt/gentoo curl -s -o /etc/syslog-ng/syslog-ng.conf https://stelfox.net/note_files/syslog-ng/syslog-ng.conf
