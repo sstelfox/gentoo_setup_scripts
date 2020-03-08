05d5846924a6f3a440ee9ee7befa215f17709a57
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2020-03-07 23:51:22 -0600

    more init stuff

--- extra_scripts/mkinit.sh
+++ extra_scripts/mkinit.sh
@@ -2,18 +2,19 @@
 
 set -o errexit
 
+INITRAMFS_PKG_ROOT=/usr/src/initpkgs
 INITRAMFS_DIR=/usr/src/initramfs
 
 rm -rf ${INITRAMFS_DIR}
 mkdir -p ${INITRAMFS_DIR}
 
-FEATURES="-*" USE="-* ipv6 make-symlinks selinux static" emerge --root=${INITRAMFS_DIR} --root-deps=rdeps --sysroot=${INITRAMFS_DIR} sys-apps/busybox
-FEATURES="-*" USE="-* selinux static thin udev" emerge --root=${INITRAMFS_DIR} --root-deps=rdeps --sysroot=${INITRAMFS_DIR} sys-fs/lvm2
+PYTHON_TARGETS="python3_6" FEATURES="-*" USE="-* ipv6 make-symlinks python selinux static" emerge --root=${INITRAMFS_PKG_ROOT} --root-deps=rdeps --sysroot=${INITRAMFS_PKG_ROOT} sys-apps/busybox
+PYTHON_TARGETS="python3_6" FEATURES="-*" USE="-* python selinux static static-libs thin udev" emerge --root=${INITRAMFS_PKG_ROOT} --root-deps=rdeps --sysroot=${INITRAMFS_PKG_ROOT} sys-fs/lvm2
 
-#FEATURES="-*" USE="-* minimal multicall static" emerge --root=${INITRAMFS_DIR} --root-deps=rdeps --sysroot=${INITRAMFS_DIR} net-misc/dropbear
-#FEATURES="-*" USE="-* static" emerge --root=${INITRAMFS_DIR} --root-deps=rdeps --sysroot=${INITRAMFS_DIR} dev-util/strace
+#FEATURES="-*" USE="-* minimal multicall static" emerge --root=${INITRAMFS_PKG_ROOT} --root-deps=rdeps --sysroot=${INITRAMFS_PKG_ROOT} net-misc/dropbear
+#FEATURES="-*" USE="-* static" emerge --root=${INITRAMFS_DIR} --root-deps=rdeps --sysroot=${INITRAMFS_PKG_ROOT} dev-util/strace
 
-mkdir -p /usr/src/initramfs/{dev,proc,root,sys,sysroot}
+mkdir -p /usr/src/initramfs/{bin,dev,etc,proc,root,sbin,sys,sysroot,tmp}
 
 # May need tty,vda...
 mknod --context=system_u:object_r:console_device_t --mode=u=rw,g=r,o=r ${INITRAMFS_DIR}/dev/console c 5 1
