c351bd6d4540286d1263e072536f9bdefdaa56c6
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2020-03-07 23:26:06 -0600

    starting the init stuff up

--- extra_scripts/mkinit.sh
+++ extra_scripts/mkinit.sh
@@ -2,43 +2,33 @@
 
 set -o errexit
 
-# Alright here is what I'm doing to build this up, it's a slow reverse engineer
-# and digging through various references I've built up over time.
-#
-# mkdir -p /tmp/init_tmp
-# cd /tmp/init_tmp
-# file /boot/initramfs-current.img
-# lz4cat /boot/initramfs-current.img | cpio -mvid
-# ls -l
+INITRAMFS_DIR=/usr/src/initramfs
 
-mkdir -p /tmp/init/
+rm -rf ${INITRAMFS_DIR}
+mkdir -p ${INITRAMFS_DIR}
 
-FEATURES="-*" USE="-* ipv6 make-symlinks static" emerge --root=/tmp/init/ --root-deps=rdeps --sysroot=/tmp/init/ sys-apps/busybox
-FEATURES="-*" USE="-* minimal multicall static" emerge --root=/tmp/init/ --root-deps=rdeps --sysroot=/tmp/init/ net-misc/dropbear
+FEATURES="-*" USE="-* ipv6 make-symlinks selinux static" emerge --root=${INITRAMFS_DIR} --root-deps=rdeps --sysroot=${INITRAMFS_DIR} sys-apps/busybox
+FEATURES="-*" USE="-* selinux static thin udev" emerge --root=${INITRAMFS_DIR} --root-deps=rdeps --sysroot=${INITRAMFS_DIR} sys-fs/lvm2
 
-# While diagnosing some issues I found this to be useful:
-FEATURES="-*" USE="-* static" emerge --root=/tmp/init/ --root-deps=rdeps --sysroot=/tmp/init/ dev-util/strace
+#FEATURES="-*" USE="-* minimal multicall static" emerge --root=${INITRAMFS_DIR} --root-deps=rdeps --sysroot=${INITRAMFS_DIR} net-misc/dropbear
+#FEATURES="-*" USE="-* static" emerge --root=${INITRAMFS_DIR} --root-deps=rdeps --sysroot=${INITRAMFS_DIR} dev-util/strace
 
-mkdir -p /tmp/init/dev
+mkdir -p /usr/src/initramfs/{dev,proc,root,sys,sysroot}
 
-# ls -l ./dev
-# file /dev/console
-# ls -lhZ /dev/console
+# May need tty,vda...
+mknod --context=system_u:object_r:console_device_t --mode=u=rw,g=r,o=r ${INITRAMFS_DIR}/dev/console c 5 1
+mknod --context=system_u:object_r:kmsg_device_t --mode=u=rw,g=r,o=r ${INITRAMFS_DIR}/dev/kmsg c 1 11
+mknod --context=system_u:object_r:null_device_t --mode=u=rw,g=rw,o=rw ${INITRAMFS_DIR}/dev/null c 1 3
+mknod --context=system_u:object_r:random_device_t --mode=u=rw,g=rw,o=rw ${INITRAMFS_DIR}/dev/random c 1 8
+mknod --context=system_u:object_r:urandom_device_t --mode=u=rw,g=rw,o=rw ${INITRAMFS_DIR}/dev/urandom c 1 9
 
-mknod --context=system_u:object_r:console_device_t --mode=u=rw,g=r,o=r /tmp/init/dev/console c 5 1
+cat << 'EOF' > ${INITRAMFS_DIR}/init
+#!/bin/busybox sh
 
-# file /dev/kmsg
-# ls -lhZ /dev/kmsg
+mount -t proc none /proc
+mount -t sysfs none /sys
 
-mknod --context=system_u:object_r:kmsg_device_t --mode=u=rw,g=r,o=r /tmp/init/dev/kmsg c 1 11
+exec sh
+EOF
 
-# ...
-
-mknod --context=system_u:object_r:null_device_t --mode=u=rw,g=rw,o=rw /tmp/init/dev/null c 1 3
-mknod --context=system_u:object_r:random_device_t --mode=u=rw,g=rw,o=rw /tmp/init/dev/random c 1 8
-mknod --context=system_u:object_r:urandom_device_t --mode=u=rw,g=rw,o=rw /tmp/init/dev/urandom c 1 9
-
-chown -R root:root /tmp/init/dev
-
-cd /tmp/init
-find . | cpio -ov | lz4c > /tmp/built_init
+chmod +x ${INITRAMFS_DIR}/init
