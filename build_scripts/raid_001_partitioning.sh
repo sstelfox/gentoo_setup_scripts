b7bd4bea674fd2a42c3a5211e7a3406a91e036d0
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2020-05-28 18:16:59 -0500

    hardcoded values

--- build_scripts/raid_001_partitioning.sh
+++ build_scripts/raid_001_partitioning.sh
@@ -101,7 +101,7 @@ dd if=/dev/zero bs=1M count=1 of=${DISK_TWO}2 oflag=sync status=none
 dd if=/dev/zero bs=1M count=16 of=${DISK_TWO}3 oflag=sync status=none
 
 # Create our raid array
-mdadm --create /dev/md0 --level=raid1 --raid-devices=2 --metadata=1.2 /dev/vda3 /dev/vdb3 &> /dev/null
+mdadm --create /dev/md0 --level=raid1 --raid-devices=2 --metadata=1.2 ${DISK_ONE}3 ${DISK_TWO}3 &> /dev/null
 /bin/dd bs=1M count=4 status=none if=/dev/zero of=/dev/md0 oflag=sync
 PARTED_BASE_RAID_CMD="/usr/sbin/parted /dev/md0 --script --align optimal --machine --"
 
